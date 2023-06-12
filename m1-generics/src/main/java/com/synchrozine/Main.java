e207bc96ceda19e10fd569077c23982c6e6d9a29
Baturay Gok <baturay.gok@gmail.com>
2023-06-12 02:20:16 -0500

    Generics

--- m1-generics/src/main/java/com/synchrozine/Main.java
+++ m1-generics/src/main/java/com/synchrozine/Main.java
@@ -1,7 +1,25 @@
 package com.synchrozine;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        //  items.add("Apple");
+
+        printDoubled(items);
+
     }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (Integer each : items) {
+            System.out.println(each * 2);
+        }
+    }
+
+
 }
