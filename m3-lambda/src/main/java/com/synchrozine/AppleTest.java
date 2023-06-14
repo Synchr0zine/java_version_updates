c7220f64e9413807af2f037d3aa67e12fc51a080
Baturay Gok <baturay.gok@gmail.com>
2023-06-14 03:16:44 -0500

    Lambda Expression, Apple Task

---
+++ m3-lambda/src/main/java/com/synchrozine/AppleTest.java
@@ -0,0 +1,34 @@
+package com.synchrozine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+        List<Apple> apples = new ArrayList<Apple>();
+        apples.add(new Apple(300, Color.GREEN));
+        apples.add(new Apple(100, Color.RED));
+        apples.add(new Apple(200, Color.GREEN));
+        apples.add(new Apple(50, Color.RED));
+
+        List<Apple> heavyApples = filterApples(apples, new AppleHeavyPredicate());
+        System.out.println(heavyApples);
+        List<Apple> greenApples = filterApples(apples, new AppleGreenColorPredicate());
+        System.out.println(greenApples);
+
+
+    }
+
+    private static List<Apple> filterApples(List<Apple> apples, ApplePredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : apples) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+}
