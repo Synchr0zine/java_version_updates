b98c1af8bae320c0ef98aa14b755d144ffa0c5b2
Baturay Gok <baturay.gok@gmail.com>
2023-06-26 03:11:41 -0500

    Stream

---
+++ m6-stream/src/main/java/org/synchrozine/Course.java
@@ -0,0 +1,14 @@
+package org.synchrozine;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Course {
+
+    private String courseName;
+    private int courseId;
+}
---
+++ m6-stream/src/main/java/org/synchrozine/Main.java
@@ -0,0 +1,29 @@
+package org.synchrozine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Stream from array
+        String[] courses = {"Java", "Spring", "Agile"};
+
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        //Stream from collection
+        List<String> courseList = Arrays.asList("Java", "Spring", "Agile");
+        Stream<String> courseStream2 = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java", 100),
+                new Course("DS", 101),
+                new Course("Microservices", 102)
+        );
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
+    }
+}
